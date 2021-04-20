285c0572bf3fc52196f32e54e163776ff35836e7
elizabethhillman <42414340+elizabethhillman@users.noreply.github.com>
2021-04-19 20:21:15 -0700

    added variables for forms.py

--- app/forms.py
+++ app/forms.py
@@ -5,5 +5,8 @@ from wtforms.validators import DataRequired
 class MessageForm(FlaskForm):
     # add
     # author (string) validator should make this textbox required
+    author = StringField('Author', validators = [DataRequired()])
     # message (string) validator should make this textbox required
+    message = StringFiels('Message', validators = [DataRequired()])
     # submit (button) text should say 'Send' 
+    submit = SubmitField('Send')
