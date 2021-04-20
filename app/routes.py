dbd3f47e485f810e6be31fd6353936533ce10914
elizabethhillman <42414340+elizabethhillman@users.noreply.github.com>
2021-04-20 15:18:49 -0700

    added route decorator and checked user in database

--- app/routes.py
+++ app/routes.py
@@ -6,14 +6,23 @@ from app.forms import MessageForm
 from app.models import User, Messages
 
 # add route '/' and also add the two methods to handle request: 'GET' and 'POST'
+@myapp_obj.route('/', methods = ['GET', 'POST'])
 def home():
     form=MessageForm()
     if form.validate_on_submit():
         # check if user exits in database
+        user = User.query.filter_by(author=form.author.data).first()
         # if not create user and add to database
+        if user is None:
+            user = User(author=form.author.data)
+            db.session.add(user)
         # create row in Message table with user (created/found) add to ta database
+        row = Messages(message = form.message.data, user_id = user.id)
 
-    posts = []
+    posts = [
+        { 'author' : 'Carlos','message': 'Yo! Where you at?!'},
+        { 'author':'Jerry', 'message':'Home. You?'}
+        ]
     # output all messages
     # create a list of dictionaries with the following structure
     # [{'author':'carlos', 'message':'Yo! Where you at?!'},
