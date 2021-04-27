211a7e56bce131253fc3bb3b02d6c0c748c28d5f
elizabethhillman <42414340+elizabethhillman@users.noreply.github.com>
2021-04-26 21:09:20 -0700

    added comments

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
 
+'''this method creates a form for author, message and the submit button. gives parameters for string field to be required and the submit field to read Send''' 
 class MessageForm(FlaskForm):
     # add
     # author (string) validator should make this textbox required
